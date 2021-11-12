ebf82fe54178d84796393cce93db6270e2899df7
Maciej Małecki <me@mmalecki.com>
2021-11-12 20:40:07 +0100

    Add a close window keybinding for ergo keyboards

--- rc.lua
+++ rc.lua
@@ -425,6 +425,8 @@ clientkeys = gears.table.join(
         {description = "toggle fullscreen", group = "client"}),
     awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
               {description = "close", group = "client"}),
+    awful.key({ modkey, "Control"   }, "Escape", function (c) c:kill()                       end,
+             {description = "close", group = "client"}),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
               {description = "toggle floating", group = "client"}),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
